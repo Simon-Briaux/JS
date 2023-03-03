cd0dc65b5a5bc1d283a15757355b60a34047285b
Simon-Briaux <113522528+Simon-Briaux@users.noreply.github.com>
2023-03-03 16:50:12 +0100

    Add files via upload

---
+++ alex.php
@@ -0,0 +1,11 @@
+
+
+<?php
+
+echo "Coucou bienvenue en enfer";
+
+?>
+
+<script src="main2.js">
+
+</script>
---
+++ main2.js
@@ -0,0 +1,34 @@
+/*var Mathias = "Simon";
+var Num = "One Piece";
+var Tab = ['FanService','animation moyen'];
+var Objet = window
+
+    alert( Mathias +" adore "+Num+" avec le "+Tab[0] +" surtout son "+Tab[1]);
+    alert(Objet.location.pathname);
+*/
+//-----------------------------------------//
+/*
+var String = "Steven";
+var Num = prompt("entrez un nombre",500);
+if(String == "Steven" && Num != 1000 || Num <= 500){
+    alert("je suis dans le cas 1");
+}else{
+    alert("je suis un kiwi");
+}
+*/
+//----------------------------
+/*
+if(outerHeight <= 800 && outerWidth <= 800){
+    alert("ALERTE");
+}else{
+    alert("TOUT VA BIEN");
+}
+*/
+
+
+var Num = prompt("entrez votre N");
+
+for(var i = 0; i<Num ;i++){
+   var result = Num * (i+1);
+} 
+alert("Le résultat de votre suite est "+result);
